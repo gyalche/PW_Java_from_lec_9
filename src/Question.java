f3bf3769d13bf458cb913c4a6672dafe00bdec4b
Dawa <sherpadawa01@outlook.com>
2022-11-03 12:13:17 +0545

    question solved on powserbase

--- src/Question.java
+++ src/Question.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
 public class Question {
+
+    //given 2 number a and b. Find a raise to the power b;
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b=sc.nextInt();
+        int ans=1;
+        for(int  i=1; i<=b; i++){
+            ans *=a;
+        }
+        System.out.println(ans);
+    }
 }
