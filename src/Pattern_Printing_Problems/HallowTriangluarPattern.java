3902a3b8becae09fa1850909f073bfd12ab58a09
Dawa <sherpadawa01@outlook.com>
2022-11-05 22:03:32 +0545

    pyramid pattern number

---
+++ src/Pattern_Printing_Problems/HallowTriangluarPattern.java
@@ -0,0 +1,14 @@
+package Pattern_Printing_Problems;
+
+import java.util.Scanner;
+
+//    1
+//   2  2
+//  3    3
+// 44444444
+public class HallowTriangluarPattern {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
--- src/Pattern_Printing_Problems/NumericTriangularPattern.java
+++ src/Pattern_Printing_Problems/NumericTriangularPattern.java
@@ -1,8 +1,21 @@
 package Pattern_Printing_Problems;
+
+import java.util.Scanner;
+
 //print
 //1
 //12
 //123
 //1234
 public class NumericTriangularPattern {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int r=sc.nextInt();
+        for(int i=1; i<=r; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
 }
