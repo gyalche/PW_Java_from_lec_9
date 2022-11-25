c822055afb6641c442e5cd27c562786043f2ef1f
Dawa <sherpadawa01@outlook.com>
2022-11-25 12:02:44 +0545

    two pointer sqauredecreasingorder

--- src/twoPointerArray/squareInNondecreaingOrder.java
+++ src/twoPointerArray/squareInNondecreaingOrder.java
@@ -2,10 +2,28 @@ package twoPointerArray;
 
 //Given an integer array 'a' sorted in non-decreaing order, return an array of the square of each number;
 //sorted in none-decreasing order;
-
 import java.util.Scanner;
 
 public class squareInNondecreaingOrder {
+    static void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    static void reverse(int[] arr){
+        int i=0; int j=arr.length-1;
+        while(i<j){
+            swap(arr, i, j);
+            i++;
+            j--;
+        }
+
+    }
+    static void printArray(int[] arr){
+        for(int i=0; i<arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
     static int[] sortSquare(int[] arr){
         int n=arr.length;
         int left=0;
@@ -25,18 +43,16 @@ public class squareInNondecreaingOrder {
         return ans;
     }
 
-    static void printArray(int[] arr){
-        for(int i=0; i<arr.length; i++){
-            System.out.println(arr[i]);
-        }
-    }
+
+
     public static void main(String[] args) {
           Scanner sc=new Scanner(System.in);
           int [] arr = {-10,-3,-2,1,4,5};
         System.out.println("Original Array");
         printArray(arr);
         System.out.println("Soreted array");
-        int[] ans = sortSquare(arr);
+        int[]ans=sortSquare(arr);
         printArray(ans);
+
     }
 }
