2ade4deb29ae3b13f35a747bc15b0f4d2566b176
Dawa <sherpadawa01@outlook.com>
2022-11-30 14:25:42 +0545

    two sum

--- src/LeetCode/TwoSum.java
+++ src/LeetCode/TwoSum.java
@@ -9,7 +9,7 @@ public class TwoSum {
     static int[] twoSum(int[] nums, int target){
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; i<nums.length; j++){
-                if(nums[i] + nums[i]==target){
+                if(nums[i] + nums[j]==target){
                     int[] a={i,j};
                     return a;
                 }
