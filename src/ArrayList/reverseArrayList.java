892b9bfaca9b47265fa221ad1b97d8610fe8808d
Dawa <sherpadawa01@outlook.com>
2022-12-11 22:33:46 +0545

    reverse ArrayList

--- src/ArrayList/reverseArrayList.java
+++ src/ArrayList/reverseArrayList.java
@@ -1,7 +1,35 @@
 package ArrayList;
 
+import java.util.ArrayList;
+
 public class reverseArrayList {
+
+    static void reverseArray(ArrayList<Integer> list){
+        int start=0;
+        int end=list.size()-1;
+
+        while(start<end){
+           Integer temp=Integer.valueOf(list.get(start));
+           list.set(start, list.get(end));
+            list.set(end, temp);
+            start++;
+            end--;
+        }
+
+    }
     public static void main(String[] args) {
+
+        ArrayList<Integer> l=new ArrayList<>();
+        l.add(0);
+        l.add(10);
+        l.add(3);
+        l.add(5);
+        l.add(22);
+        l.add(10);
+
+        System.out.println("Original list " + l);
+        reverseArray(l);
+        System.out.println("After reverse " + l);
 
     }
 }
