a8bf2a5cfb8a475f47afa8b85cc18b95b11c2e6e
Dawa <sherpadawa01@outlook.com>
2023-01-12 14:07:46 +0545

    completed palindrom using recursion

---
+++ src/recursion/String/Palindrom.java
@@ -0,0 +1,26 @@
+package recursion.String;
+
+import java.util.Scanner;
+
+public class Palindrom {
+    static String reverse(String s, int idx){
+        if(idx==s.length()) return "";
+
+        String smallAns=reverse(s, idx+1);
+
+        return smallAns + s.charAt(idx);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter word to check whether its palindrom or not");
+        String s=sc.nextLine();
+        String rev=reverse(s, 0);
+
+        if(rev.equals(s)){
+            System.out.printf("%s is Palindrom", s);
+        }else{
+            System.out.printf("%s is Not Palindrom", s);
+        }
+
+    }
+}
